6da2ef55e0af5595cf8f5c5dfb33ee9a89db2b42
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-22 20:12:10 -0500

    Update SunkenSword.cs
    Made particles better

--- Content/Items/Weapons/SunkenSword.cs
+++ Content/Items/Weapons/SunkenSword.cs
@@ -45,11 +45,14 @@ namespace ArcaneOdyssey.Content.Items.Weapons
             {
                 player.AddBuff(ModContent.BuffType<RisenTide>(), (int)(60 * (CurrentImbue is not null ? CurrentImbue.AOImbueSpeed : 1) * AOSpeed * 5));
                 SoundEngine.PlaySound(SoundID.Splash, player.position);
+                player.velocity.Y *= 0.1f;
                 player.velocity.Y -= 20;
                 // Adds dust
                 for(int dustCountInt = 0;dustCountInt<50;dustCountInt++){
-                    Dust.NewDust(player.position+new Vector2(-20f+(40f*(float)(System.Math.Sin((double)dustCountInt*3.0))),0f),3,3,DustID.DungeonWater,0f,-0.4f * (float)dustCountInt,255,new Color(255,255,255,255),1f);
-                    Dust.NewDust(player.position+new Vector2(20f+(40f*(float)(System.Math.Sin(((double)dustCountInt*3.0)+(3.14)))),0f),3,3,DustID.DungeonWater,0f,-0.4f * (float)dustCountInt,255,new Color(255,255,255,255),1f);
+                    Dust.NewDust(player.position+new Vector2(-20f+(40f*(float)(System.Math.Sin((double)dustCountInt*3.0))),0f),3,3,DustID.Water,0f,-1f * (float)dustCountInt,255,new Color(255,255,255,255),1.3f);
+                    Dust.NewDust(player.position+new Vector2(20f+(40f*(float)(System.Math.Sin(((double)dustCountInt*3.0)+(3.14)))),0f),3,3,DustID.Water,0f,-1f * (float)dustCountInt,255,new Color(255,255,255,255),1.3f);
+                    Dust.NewDust(player.position+new Vector2(-20f+(40f*(float)(System.Math.Sin((double)dustCountInt*3.0))),0f),3,3,DustID.DungeonWater,0f,-0.5f * (float)dustCountInt,255,new Color(255,255,255,255),1f);
+                    Dust.NewDust(player.position+new Vector2(20f+(40f*(float)(System.Math.Sin(((double)dustCountInt*3.0)+(3.14)))),0f),3,3,DustID.DungeonWater,0f,-0.5f * (float)dustCountInt,255,new Color(255,255,255,255),1f);
                 }
             }
             return null;
