354fb102014a80bea1b6c6ef8064f3afa9a7de5e
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-23 18:14:33 -0500

    Create HecateShard.cs

---
+++ Content/Items/HecateShard.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static ArcaneOdyssey.AOUtils;
+
+namespace ArcaneOdyssey.Content.Items
+{
+    public class HecateShard : ModItem
+    {
+        public int AOValue = 20000;
+        public int AORarity = AORarities.Rare;
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.rare = AORarity;
+            Item.value = GalleonToCopper(AOValue, Item.rare);
+        }
+    }
+}
