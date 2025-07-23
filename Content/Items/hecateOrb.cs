2daaa1646aeb25c498ca0a78a638d6766fe063eb
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-23 00:08:05 -0500

    Create hecateOrb.cs

---
+++ Content/Items/hecateOrb.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static ArcaneOdyssey.AOConversion;
+
+namespace ArcaneOdyssey.Content.Items
+{
+    public class HecateOrb : ModItem
+    {
+        public int AOValue = 10000;
+        public int AORarity = AORarities.Rare;
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 9999;
+            Item.rare = AORarity;
+            Item.value = GalleonToCopper(AOValue, Item.rare);
+        }
+    }
+}
