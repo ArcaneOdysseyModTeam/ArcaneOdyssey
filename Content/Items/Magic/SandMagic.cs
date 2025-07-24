afb9302c28e0d6fd5be4c5d25b422dffc7778faa
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-23 21:27:55 -0500

    Add files via upload

---
+++ Content/Items/Magic/SandMagic.cs
@@ -0,0 +1,34 @@
+using ArcaneOdyssey.Content.Items.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static ArcaneOdyssey.AOUtils;
+
+namespace ArcaneOdyssey.Content.Items.Magic
+{
+	public class SandMagic : AOMagic
+	{
+		public override float AOImbueSpeed => 1f;
+		public override float AOImbueSize => 1f;
+		public override float AOImbueDamage => 1f;
+		public override AODebuff? MagicDebuff => new AODebuff(BuffID.OnFire3, 60*10);
+		public override MagicEffects Effects => new MagicEffects(
+			[ // these are debuffs cleared on hit
+				
+			], 
+			[
+				
+			]
+			);
+			public override void AddRecipes() {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<HecateOrb>(1);
+            recipe.Register();
+        }
+	}
+}
