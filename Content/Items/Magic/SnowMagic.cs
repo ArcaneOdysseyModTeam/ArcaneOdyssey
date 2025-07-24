33efd4695fa4ee87e283ccb6fa4da30c7e0810c0
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-23 23:06:33 -0500

    Update SnowMagic.cs

--- Content/Items/Magic/SnowMagic.cs
+++ Content/Items/Magic/SnowMagic.cs
@@ -19,7 +19,7 @@ namespace ArcaneOdyssey.Content.Items.Magic
 		public override float AOMagicSpeed => 1.1f;
 		public override float AOMagicSize => 1.15f;
 		public override float AOMagicDamage => 0.925f;
-		public override AODebuff? MagicDebuff => new AODebuff(ModContent.BuffType<SnowyEffect>(), 60*10);
+		public override AODebuff? MagicDebuff => new AODebuff(BuffID.Chilled, 60*10);
 		public override MagicEffects Effects => new MagicEffects(
 			[ // these are debuffs cleared on hit
 				
