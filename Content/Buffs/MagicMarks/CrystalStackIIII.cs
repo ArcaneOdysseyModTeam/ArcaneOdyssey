75b55bfa76aa4338dc8f130568124bc77fbaf62e
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-26 18:46:04 -0500

    Update CrystalStackIIII.cs

--- Content/Buffs/MagicMarks/CrystalStackIIII.cs
+++ Content/Buffs/MagicMarks/CrystalStackIIII.cs
@@ -15,6 +15,7 @@ namespace ArcaneOdyssey.Content.Buffs.MagicMarks
 {
     public class CrystalStackIIII : MagicMark {
          public override void Update(NPC npc, ref int buffIndex) {
+            //add dust later
             for(int i = 0;i<NPC.maxBuffs;i++) {
                 if(npc.buffType[i] == ModContent.BuffType<CrystalStackI>() || npc.buffType[i] == ModContent.BuffType<CrystalStackII>() || npc.buffType[i] == ModContent.BuffType<CrystalStackIII>() || npc.buffType[i] == ModContent.BuffType<CrystalStackIIII>()){
                     npc.DelBuff(i);
