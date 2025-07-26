f8b4efcee6314e811793170d22b2ec91406ddaae
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-25 22:27:48 -0500

    Create ArmegeddonMagic.cs

---
+++ Content/Items/Magic/ArmegeddonMagic.cs
@@ -0,0 +1,36 @@
+using ArcaneOdyssey.Content.Items.Base;
+using System;
+using System.Collections.Generic;
+using ArcaneOdyssey.Content.Buffs.Stuns;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static ArcaneOdyssey.AOUtils;
+
+namespace ArcaneOdyssey.Content.Items.Magic
+{
+	public class ArmegeddonMagic : AOMagic
+	{
+		public override float AOImbueSpeed => 1f;
+		public override float AOImbueSize => 1f;
+		public override float AOImbueDamage => 1f;
+		public override float AOMagicSpeed => 1f;
+		public override float AOMagicSize => 1f;
+		public override float AOMagicDamage => 1f;
+		public override AODebuff? MagicDebuff => new AODebuff(ModContent.BuffType<AOPetrified>(),10*60);
+		public override MagicEffects Effects => new MagicEffects(
+			[ // these are debuffs cleared on hit
+				
+			], 
+			[
+				
+			]
+			);
+			public override void AddRecipes() {
+            
+        }
+	}
+}
