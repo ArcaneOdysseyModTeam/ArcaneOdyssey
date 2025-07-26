bc4ea86e91406746fca00b3ad891cc4e7bb4688b
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-25 21:46:10 -0500

    Add files via upload

---
+++ Content/Items/Magic/PoisonLightningMagic.cs
@@ -0,0 +1,35 @@
+using ArcaneOdyssey.Content.Items.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static ArcaneOdyssey.AOUtils;
+
+namespace ArcaneOdyssey.Content.Items.Magic
+{
+	public class PoisonLightningMagic : AOMagic
+	{
+		public override float AOImbueSpeed => 1f;
+		public override float AOImbueSize => 1f;
+		public override float AOImbueDamage => 1f;
+		public override float AOMagicSpeed => 1f;
+		public override float AOMagicSize => 1f;
+		public override float AOMagicDamage => 1f;
+		public override AODebuff? MagicDebuff => null;
+		public override MagicEffects Effects => new MagicEffects(
+			[ // these are debuffs cleared on hit
+				
+			], 
+			[
+				
+			]
+			);
+			public override void AddRecipes() {
+            
+        }
+	}
+}
