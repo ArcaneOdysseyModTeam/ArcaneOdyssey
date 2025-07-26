956329cee9eba250718e744b54c12155dc966e95
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-07-25 22:36:49 -0500

    Update VesuviusMagic.cs

--- Content/Items/Magic/VesuviusMagic.cs
+++ Content/Items/Magic/VesuviusMagic.cs
@@ -14,12 +14,12 @@ namespace ArcaneOdyssey.Content.Items.Magic
 {
 	public class VesuviusMagic : AOMagic
 	{
-		public override float AOImbueSpeed => 1f;
-		public override float AOImbueSize => 1f;
-		public override float AOImbueDamage => 1f;
-		public override float AOMagicSpeed => 1f;
-		public override float AOMagicSize => 1f;
-		public override float AOMagicDamage => 1f;
+		public override float AOImbueSpeed => 0.9f;
+		public override float AOImbueSize => 6f;
+		public override float AOImbueDamage => 3f;
+		public override float AOMagicSpeed => 0.9f;
+		public override float AOMagicSize => 6f;
+		public override float AOMagicDamage => 3f;
 		public override AODebuff? MagicDebuff => new AODebuff(ModContent.BuffType<AOPetrified>(),10*60);
 		public override AODebuff? MagicDebuff2 => new AODebuff(BuffID.OnFire3,10*60);
 		public override MagicEffects Effects => new MagicEffects(
