d25d7968b357e45a92c7b48cb4c200c485395c1b
Guard Boi <105892653+guardboi@users.noreply.github.com>
2025-08-19 13:01:10 -0700

    oops

--- Content/Projectiles/Weapons/SunkenStaffProjectile.cs
+++ Content/Projectiles/Weapons/SunkenStaffProjectile.cs
@@ -49,7 +49,7 @@ namespace ArcaneOdyssey.Content.Projectiles.Weapons
 
 		public override void EffectBeforeSpin(Player player, float spintime)
 		{
-			if (Main.player[Projectile.owner] == Main.clientPlayer)
+			if (Projectile.owner == Main.myPlayer)
 				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity, ModContent.ProjectileType<FuryoftheSea>(), Projectile.damage, 0f, Projectile.owner, ai1: MathHelper.TwoPi * 2f / spintime * player.direction);
 		}
 	}
