3da643737d5ea29da2a43f4bac11885b9b1a5718
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-09-03 18:25:29 -0500

    magic circle work

---
+++ Content/Projectiles/MagicCircle.cs
@@ -0,0 +1,33 @@
+using ArcaneOdyssey.Content.Items.Base;
+using ArcaneOdyssey.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Terraria.ModLoader;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ArcaneOdyssey.Content.Projectiles
+{
+	public abstract class MagicCircle : ModProjectile
+	{
+		
+		public override void SetDefaults()
+		{
+			Projectile.friendly = true;
+            Main.projFrames[Projectile.type] = 4;
+		}
+        public override void AI() {
+            if (++Projectile.frame >= Main.projFrames[Projectile.type]) {
+					Projectile.frame = 0;
+            }
+            Projectile.frame++;
+        }
+	}
+}
