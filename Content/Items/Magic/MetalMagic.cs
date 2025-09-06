1d76ee1e457fdb60d4153f086fbe6caa4b658c6a
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-09-06 18:54:56 -0500

    Metal magic

--- Content/Items/Magic/MetalMagic.cs
+++ Content/Items/Magic/MetalMagic.cs
@@ -13,11 +13,15 @@ using Terraria.ModLoader;
 using static ArcaneOdyssey.AOUtils;
 using ArcaneOdyssey.Content.Items.Materials;
 using ArcaneOdyssey.Content.Buffs.DOT;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
 
 namespace ArcaneOdyssey.Content.Items.Magic
 {
 	public class MetalMagic : AOMagic
 	{
+		public override SoundStyle? MagicSound => SoundID.Item99;
+        public override Color MagicColour => new Color(100,100,100,255);
 		public override float AOImbueSpeed => 0.825f;
 		public override float AOImbueSize => 1.158f;
 		public override float AOImbueDamage => 1.1f;
@@ -42,6 +46,28 @@ namespace ArcaneOdyssey.Content.Items.Magic
 
 			]
 			);
+		public override void SpawningEffects(Projectile projectile)
+		{ 
+			for(int n = 0;n<10;n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X+(projectile.width*(float)Main.rand.NextDouble()),projectile.position.Y+(projectile.height*(float)Main.rand.NextDouble())),0,0,DustID.Mercury,(projectile.velocity.X*0.4f),(projectile.velocity.Y*0.4f),0,default,1f)];
+			}
+		}
+
+		public override void LingeringEffects(Projectile projectile)
+		{
+			Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 1, 1, DustID.SilverFlame, 0f, 0f, 0, default, 1f)];
+			spawnedDust.noGravity = true;
+		}
+
+		public override void KillEffects(Projectile projectile)
+		{
+			for (int n = 0; n < 30; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 0, 0, DustID.Mercury, (2f * (float)(Main.rand.NextDouble() - 0.5)), (2f * (float)(Main.rand.NextDouble() - 0.5)), 0, default, 1f)];
+			}
+			SoundEngine.PlaySound(MagicSound, projectile.position, null);
+		}
 				public override Dictionary<Type, int> Spells => new Dictionary<Type, int>([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<MetalBlast>()),]);
 		
 		public override void AddRecipes() {
