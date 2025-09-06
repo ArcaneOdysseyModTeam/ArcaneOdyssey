397fd5945b6c419549256d007d4c331cfcba88e3
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-09-06 17:19:57 -0500

    Lightning

--- Content/Items/Magic/LightningMagic.cs
+++ Content/Items/Magic/LightningMagic.cs
@@ -14,11 +14,15 @@ using Terraria.ModLoader;
 using static ArcaneOdyssey.AOUtils;
 using ArcaneOdyssey.Content.Items.Materials;
 using ArcaneOdyssey.Content.Buffs.DOT;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
 
 namespace ArcaneOdyssey.Content.Items.Magic
 {
     public class LightningMagic : AOMagic
     {
+        public override SoundStyle? MagicSound => SoundID.DD2_LightningAuraZap;
+        public override Color MagicColour => new Color(255,140,255,255);
         public override float AOImbueSpeed => 1.2f;
         public override float AOImbueSize => .95f;
         public override float AOImbueDamage => .95f;
@@ -50,6 +54,29 @@ namespace ArcaneOdyssey.Content.Items.Magic
 				new MagicBuffMultiplier(ModContent.BuffType<CrystalStackIIII>(),1.075f)
             ]
             );
+            public override void SpawningEffects(Projectile projectile) 
+			{
+			for (int n = 0; n < 3; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 0, 0, DustID.WitherLightning, (projectile.velocity.X * 0.2f), (projectile.velocity.Y * 0.2f), 0, default, 1.2f)];
+			}
+			}
+		public override void LingeringEffects(Projectile projectile)
+		{
+            Dust spawnedDust = Dust.NewDustPerfect(projectile.position+(new Vector2(Vector2.Normalize(projectile.velocity).Y,(Vector2.Normalize(projectile.velocity).X*-1)*(((float)Main.time%2))*projectile.height*8f)+new Vector2(0f,projectile.height/2f)), DustID.CrystalPulse, null, 255, default, 1.2f);
+            spawnedDust.noGravity = true;
+
+			Dust spawnedDust2 = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 1, 1, DustID.WitherLightning, 0f, 0f, 0, default, 0.3f)];
+		}
+
+		public override void KillEffects(Projectile projectile)
+		{
+			for (int n = 0; n < 10; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 0, 0, DustID.WitherLightning, (8f * (float)(Main.rand.NextDouble() - 0.5)), (8f * (float)(Main.rand.NextDouble() - 0.5)), 0, default, 1.2f)];
+			}
+			SoundEngine.PlaySound(MagicSound, projectile.position, null);
+		}
             public override Dictionary<Type, int> Spells => new Dictionary<Type, int>([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<LightningBlast>()),]);
 		
 		public override void AddRecipes() {
