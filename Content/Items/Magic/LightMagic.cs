71fa3c0927dd0d4659bfe12d8ec942e7c8931095
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-09-05 22:02:45 -0500

    Light magic

--- Content/Items/Magic/LightMagic.cs
+++ Content/Items/Magic/LightMagic.cs
@@ -12,11 +12,15 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static ArcaneOdyssey.AOUtils;
 using ArcaneOdyssey.Content.Items.Materials;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
 
 namespace ArcaneOdyssey.Content.Items.Magic
 {
 	public class LightMagic : AOMagic
 	{
+		public override SoundStyle? MagicSound => SoundID.Item9;
+        public override Color MagicColour => new Color(255,255,0,255);
 		public override float AOImbueSpeed => 1.3f;
 		public override float AOImbueSize => 0.946f;
 		public override float AOImbueDamage => 0.9f;
@@ -36,6 +40,34 @@ namespace ArcaneOdyssey.Content.Items.Magic
 				new MagicBuffMultiplier(ModContent.BuffType<CrystalStackIIII>(),1.075f)
 			]
 			);
+			public override void SpawningEffects(Projectile projectile) 
+			{
+			for (int n = 0; n < 3; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 0, 0, DustID.YellowStarDust, (projectile.velocity.X * 0.2f), (projectile.velocity.Y * 0.2f), 0, default, 3f)];
+				spawnedDust.noGravity = true;
+				Dust spawnedDust2 = Main.dust[Dust.NewDust(new Vector2(projectile.position.X+(projectile.width*(float)Main.rand.NextDouble()),projectile.position.Y+(projectile.height*(float)Main.rand.NextDouble())),0,0,DustID.YellowTorch,(projectile.velocity.X*0.2f),(projectile.velocity.Y*0.2f),0,default,3f)];
+				spawnedDust2.noGravity = true;
+			}
+			}
+		public override void LingeringEffects(Projectile projectile)
+		{
+			Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 1, 1, DustID.YellowStarDust, 0f, 0f, 0, default, 1f)];
+			Dust spawnedDust2 = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 1, 1, DustID.YellowTorch, 0f, 0f, 0, default, 2f)];
+			spawnedDust2.noGravity = true;
+		}
+
+		public override void KillEffects(Projectile projectile)
+		{
+			for (int n = 0; n < 10; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 0, 0, DustID.YellowStarDust, (8f * (float)(Main.rand.NextDouble() - 0.5)), (8f * (float)(Main.rand.NextDouble() - 0.5)), 0, default, 3f)];
+				spawnedDust.noGravity = true;
+				Dust spawnedDust2 = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * (float)Main.rand.NextDouble()), projectile.position.Y + (projectile.height * (float)Main.rand.NextDouble())), 0, 0, DustID.YellowTorch, (8f * (float)(Main.rand.NextDouble() - 0.5)), (8f * (float)(Main.rand.NextDouble() - 0.5)), 0, default, 3f)];
+				spawnedDust2.noGravity = true;
+			}
+			SoundEngine.PlaySound(MagicSound, projectile.position, null);
+		}
 				public override Dictionary<Type, int> Spells => new Dictionary<Type, int>([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<LightBlast>()),]);
 		
 		public override void AddRecipes() {
