52ae83cda10f0ed8ae78211db4b2482c312292c4
Guard Boi <105892653+guardboi@users.noreply.github.com>
2025-09-07 20:14:03 -0700

    vanity item cause im evil

---
+++ Content/Items/Vanity/KindraBlade.cs
@@ -0,0 +1,89 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArcaneOdyssey.Content.Items.Vanity
+{
+    public class KindraBlade : ModItem
+    {
+        public override void Load()
+        {
+            if (Main.netMode != NetmodeID.Server)
+            {
+                EquipLoader.AddEquipTexture(Mod, "PatreonWings/Items/Vanity/Kindra_Head", EquipType.Head, this);
+                EquipLoader.AddEquipTexture(Mod, "PatreonWings/Items/Vanity/Kindra_Body", EquipType.Body, this);
+                EquipLoader.AddEquipTexture(Mod, "PatreonWings/Items/Vanity/Kindra_Legs", EquipType.Legs, this);
+                EquipLoader.AddEquipTexture(Mod, "PatreonWings/Items/Vanity/Kindra_Back", EquipType.Back, this);
+            }
+        }
+
+        public override void SetStaticDefaults()
+        {
+
+            if (Main.netMode == NetmodeID.Server)
+                return;
+
+            int equipSlotHead = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
+            ArmorIDs.Head.Sets.DrawHead[equipSlotHead] = false;
+
+            int equipSlotLegs = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Legs);
+            ArmorIDs.Legs.Sets.HidesBottomSkin[equipSlotLegs] = true;
+
+            int equipSlotBody = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body);
+            ArmorIDs.Body.Sets.HidesTopSkin[equipSlotBody] = true;
+            ArmorIDs.Body.Sets.HidesArms[equipSlotBody] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 26;
+            Item.height = 38;
+            Item.accessory = true;
+            Item.value = 0;
+            Item.rare = ItemRarityID.Red;
+            Item.vanity = true;
+        }
+
+        public override void UpdateVanity(Player player)
+        {
+            player.GetModPlayer<GuardPlayer>().vanityEquipped = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!hideVisual)
+            {
+                player.GetModPlayer<GuardPlayer>().vanityEquipped = true;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().AddIngredient(ItemID.BloodButcherer).AddTile(TileID.LivingLoom).Register();
+        }
+    }
+
+    public class GuardPlayer : ModPlayer
+    {
+        public bool vanityEquipped = false;
+
+        public override void ResetEffects()
+        {
+            vanityEquipped = false;
+        }
+
+        public override void FrameEffects()
+        {
+            if (vanityEquipped)
+            {
+                Player.back = EquipLoader.GetEquipSlot(Mod, "KindraBlade", EquipType.Back);
+                Player.legs = EquipLoader.GetEquipSlot(Mod, "KindraBlade", EquipType.Legs);
+                Player.head = EquipLoader.GetEquipSlot(Mod, "KindraBlade", EquipType.Head);
+                Player.body = EquipLoader.GetEquipSlot(Mod, "KindraBlade", EquipType.Body);
+            }
+        }
+    }
+}
