af25d4905fcce049d9641d57701b3cfb079468b6
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-09-22 19:11:20 -0500

    Fixed crafting for scrolls

--- Content/Items/Scrolls/ExplosionScroll.cs
+++ Content/Items/Scrolls/ExplosionScroll.cs
@@ -7,6 +7,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using ArcaneOdyssey.Content.Items.Materials;
 using static ArcaneOdyssey.AOUtils;
 
 namespace ArcaneOdyssey.Content.Items.Scrolls
