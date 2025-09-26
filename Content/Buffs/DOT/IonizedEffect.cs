a912bf624e088ac72c16fc3f7ac4cfaf4c47feb8
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-09-25 21:42:48 -0500

    Ionized debuff completed

--- Content/Buffs/DOT/IonizedEffect.cs
+++ Content/Buffs/DOT/IonizedEffect.cs
@@ -10,12 +10,14 @@ using Microsoft.Xna.Framework;
 using static ArcaneOdyssey.AOUtils;
 using Terraria.Localization;
 using ArcaneOdyssey.Content.Buffs.Base;
+using ArcaneOdyssey.Content.Buffs.MagicMarks;
 
 namespace ArcaneOdyssey.Content.Buffs.DOT
 {
     public class IonizedEffect : AODebuff
     {
         private int frameNum = 0;
+
         public override void Update(NPC npc, ref int buffIndex)
         {
             if (!Main.dedServ)
@@ -23,6 +25,32 @@ namespace ArcaneOdyssey.Content.Buffs.DOT
                 var dust = Dust.NewDustDirect(npc.position + new Vector2((float)npc.width / 2f, (float)npc.height / 2f), 1, 1, DustID.CursedTorch, 0f, 0f, 1, default, 3f);
                 dust.velocity *= 0.8f;
             }
+            npc.lifeRegen -= 1 + (5 * GetBurnStacks(npc));
+        }
+        private int BurnStack(int buff, NPC npc)
+        {
+            if (npc.HasBuff(buff))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private int GetBurnStacks(NPC npc)
+        {
+            int burnCount = 0;
+            burnCount = 0;
+            burnCount += BurnStack(BuffID.OnFire, npc);
+            burnCount += BurnStack(BuffID.ShadowFlame, npc);
+            burnCount += BurnStack(BuffID.OnFire3, npc);
+            burnCount += BurnStack(BuffID.CursedInferno, npc);
+            burnCount += BurnStack(BuffID.Daybreak, npc);
+            burnCount += BurnStack(BuffID.Frostburn, npc);
+            burnCount += BurnStack(BuffID.Oiled, npc);
+            burnCount += BurnStack(ModContent.BuffType<CharredEffect>(), npc);
+            return burnCount;
         }
     }
 }
