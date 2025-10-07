81e05daa523409f89a79eb18484b4e1c0d042eb0
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-07 18:52:27 -0500

    Nerfed thermofist

--- Content/Items/FightingStyles/ThermoFist.cs
+++ Content/Items/FightingStyles/ThermoFist.cs
@@ -11,6 +11,7 @@ using Terraria.ModLoader;
 using static ArcaneOdyssey.AOUtils;
 using ArcaneOdyssey.Content.Items.Materials;
 using ArcaneOdyssey.Content.Buffs.DOT;
+using ArcaneOdyssey.Content.Buffs.Stuns;
 using Terraria.Audio;
 
 namespace ArcaneOdyssey.Content.Items.FightingStyles
@@ -19,11 +20,35 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 	{
 		public override Color ImbueColour => Color.White;
 
-		public override float AOImbueDamage => 1.075f;
-		public override float AOImbueSpeed => 1f;
-		public override float AOImbueSize => 1.06f;
-		public override float AOScrollDamage => .925f;
-		public override float AOScrollSize => 1f;
-		public override float AOScrollSpeed => 1f;
+		public override float AOImbueDamage => 0.85f;
+		public override float AOImbueSpeed => 1.3f;
+		public override float AOImbueSize => 0.833f;
+		public override float AOScrollDamage => .75f;
+		public override float AOScrollSize => 1.3f;
+		public override float AOScrollSpeed => 0.8f;
+
+		public override AODebuffRequirement[] ImbueDebuffs => [new(ModContent.BuffType<SearedEffect>(), 60*10)];
+		public override CombinedDebuff[] CombinedDebuffs => [new(ModContent.BuffType<CharredEffect>(), ModContent.BuffType<AOPetrified>())];
+		public override SynergyEffects Effects => new(
+			[
+				BuffID.Wet,
+				ModContent.BuffType<AOBleed>(),
+				ModContent.BuffType<FreezingEffect>()
+			],
+			[
+				new MagicBuffMultiplier(ModContent.BuffType<Crystallized>(),0.85f),
+				new MagicBuffMultiplier(ModContent.BuffType<SnowyEffect>(),0.95f),
+				new MagicBuffMultiplier(ModContent.BuffType<FreezingEffect>(),0.95f),
+				new MagicBuffMultiplier(ModContent.BuffType<AOBleed>(),1.15f),
+				new MagicBuffMultiplier(ModContent.BuffType<CharredEffect>(),1.1f),
+				new MagicBuffMultiplier(BuffID.OnFire3,1.075f),
+				new MagicBuffMultiplier(BuffID.Venom,1.075f),
+				new MagicBuffMultiplier(ModContent.BuffType<SearedEffect>(),1.1f),
+				new MagicBuffMultiplier(BuffID.ShadowFlame,1.1f),
+				new MagicBuffMultiplier(ModContent.BuffType<SandyEffect>(),0.8f),
+				new MagicBuffMultiplier(BuffID.OnFire,1.1f),
+				new MagicBuffMultiplier(ModContent.BuffType<AOScalding>(),1.1f),
+			]
+		);
 	}
 }
