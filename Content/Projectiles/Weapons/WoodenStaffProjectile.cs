3bbfd00a1b03c7a757c6955709e63f84665f4022
Guard <guardboi24@gmail.com>
2025-10-07 15:06:50 -0700

    Update WoodenStaffProjectile.cs

--- Content/Projectiles/Weapons/WoodenStaffProjectile.cs
+++ Content/Projectiles/Weapons/WoodenStaffProjectile.cs
@@ -15,7 +15,6 @@ namespace ArcaneOdyssey.Content.Projectiles.Weapons
 {
 	public class WoodenStaffProjectile : BaseStaffProjectile
 	{
-		public override bool? Cold => true;
 		public override float AOSpeed => 1.05f;
 		public override float AOSize => .9f;
 		public override float AODamage => 1f;
