860f49aff28f71d0259962f04873e2e9d72e6a2d
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-07 18:45:27 -0500

    Update SailorFist.cs

--- Content/Items/FightingStyles/SailorFist.cs
+++ Content/Items/FightingStyles/SailorFist.cs
@@ -12,6 +12,7 @@ using static ArcaneOdyssey.AOUtils;
 using ArcaneOdyssey.Content.Items.Materials;
 using ArcaneOdyssey.Content.Buffs.DOT;
 using Terraria.Audio;
+using ArcaneOdyssey.Content.Buffs.Stuns;
 
 namespace ArcaneOdyssey.Content.Items.FightingStyles
 {
@@ -19,11 +20,38 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 	{
 		public override Color ImbueColour => Color.White;
 
-		public override float AOImbueDamage => 1.075f;
+		public override float AOImbueDamage => 0.925f;
 		public override float AOImbueSpeed => 1f;
-		public override float AOImbueSize => 1.06f;
-		public override float AOScrollDamage => .925f;
-		public override float AOScrollSize => 1f;
+		public override float AOImbueSize => 1.278f;
+		public override float AOScrollDamage => .85f;
+		public override float AOScrollSize => 1.2f;
 		public override float AOScrollSpeed => 1f;
+
+		public override AODebuffRequirement[] ImbueDebuffs => [new(BuffID.Wet, 60*10)];
+		public override SynergyEffects Effects => new(
+			[
+				ModContent.BuffType<SearedEffect>(),
+				ModContent.BuffType<CharredEffect>(),
+				BuffID.OnFire,
+				BuffID.OnFire3,
+				BuffID.Venom,
+				BuffID.ShadowFlame,
+				ModContent.BuffType<AOScalding>(),
+				ModContent.BuffType<AOPetrified>()
+			],
+			[
+				new MagicBuffMultiplier(ModContent.BuffType<Crystallized>(),1.1f),
+				new MagicBuffMultiplier(ModContent.BuffType<SnowyEffect>(),1.1f),
+				new MagicBuffMultiplier(ModContent.BuffType<FreezingEffect>(),1.075f),
+				new MagicBuffMultiplier(ModContent.BuffType<AOBleed>(),1.05f),
+				new MagicBuffMultiplier(ModContent.BuffType<CharredEffect>(),0.9f),
+				new MagicBuffMultiplier(BuffID.OnFire3,0.9f),
+				new MagicBuffMultiplier(BuffID.Venom,0.9f),
+				new MagicBuffMultiplier(ModContent.BuffType<SearedEffect>(),0.85f),
+				new MagicBuffMultiplier(BuffID.ShadowFlame,0.85f),
+				new MagicBuffMultiplier(ModContent.BuffType<SandyEffect>(),0.8f),
+				new MagicBuffMultiplier(BuffID.OnFire,0.8f)
+			]
+		);
 	}
 }
