9ae5691b1f1598d4abd9d947d9948a3dec9b213e
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-06 21:22:43 -0500

    Starting on Fighting Styles

---
+++ Content/Items/FightingStyles/IronLeg.cs
@@ -0,0 +1,21 @@
+﻿using ArcaneOdyssey.Content.Items.Base;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcaneOdyssey.Content.Items.FightingStyles
+{
+    public class IronLeg : FightingStyle
+    {
+        public override Color ImbueColour => Color.White;
+
+		public override float AOImbueDamage => 1.075f;
+		public override float AOImbueSpeed => 1f;
+		public override float AOImbueSize => 1.06f;
+		public override float AOScrollDamage => .925f;
+		public override float AOScrollSize => 1f;
+	}
+}
