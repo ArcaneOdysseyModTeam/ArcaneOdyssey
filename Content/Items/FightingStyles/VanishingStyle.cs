b82ca081fd253b958faf9b70719ba38313a146b6
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-07 19:30:56 -0500

    more sfx

--- Content/Items/FightingStyles/VanishingStyle.cs
+++ Content/Items/FightingStyles/VanishingStyle.cs
@@ -18,7 +18,7 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 	public class VanishingStyle : FightingStyle
 	{
 		public override Color ImbueColour => Color.White;
-		public override SoundStyle? ImbueSound => SoundID.Item63;
+		public override SoundStyle? ImbueSound => SoundID.Item64;
 
 		public override float AOImbueDamage => 0.9f;
 		public override float AOImbueSpeed => 1.2f;
