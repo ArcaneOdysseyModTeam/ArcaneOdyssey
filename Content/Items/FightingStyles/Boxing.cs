8c78d3d797ee2eb9067c61490e72c13645deeb73
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-07 19:16:45 -0500

    Some more vfx

--- Content/Items/FightingStyles/Boxing.cs
+++ Content/Items/FightingStyles/Boxing.cs
@@ -32,5 +32,36 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 				new MagicBuffMultiplier(ModContent.BuffType<FreezingEffect>(),1.15f)
 			]
 		);
+
+		public override void SpawningEffects(Entity projectile) 
+		{
+			for (int n = 0; n<3; n++)
+			{
+					Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X+(projectile.width*Main.rand.NextFloat()),projectile.position.Y+(projectile.height*Main.rand.NextFloat())),0,0,DustID.BubbleBurst_White,(projectile.velocity.X*2f),(projectile.velocity.Y*2f),0,default,3f)];
+					spawnedDust.noGravity = true;
+			}
+		}
+		public override void LingeringEffects(Entity projectile)
+		{
+			Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * Main.rand.NextFloat()), projectile.position.Y + (projectile.height * Main.rand.NextFloat())), 1, 1, DustID.BubbleBurst_White, 0f, 0f, 0, default, 1f)];
+			spawnedDust.noGravity = true;
+		}
+		public override void ExplosionEffects(Entity projectile)
+		{
+			for (int n = 0; n < 3; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width / 2f), projectile.position.Y + (projectile.height / 2f)), 1, 1, DustID.BubbleBurst_White, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 3f)];
+				spawnedDust.noGravity = true;
+			}
+		}
+		public override void KillEffects(Entity projectile)
+		{
+			for (int n = 0; n < 10; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * Main.rand.NextFloat()), projectile.position.Y + (projectile.height * Main.rand.NextFloat())), 0, 0, DustID.BubbleBurst_White, (8f * Main.rand.NextFloat() - 0.5f), (8f * Main.rand.NextFloat() - 0.5f), 0, default, 3f)];
+				spawnedDust.noGravity = true;
+			}
+			SoundEngine.PlaySound(ImbueSound, projectile.position, null);
+		}
 	}
 }
--- Content/Items/FightingStyles/IronLeg.cs
+++ Content/Items/FightingStyles/IronLeg.cs
@@ -18,7 +18,7 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 	public class IronLeg : FightingStyle
 	{
 		public override Color ImbueColour => Color.White;
-		public override SoundStyle? ImbueSound => null;
+		public override SoundStyle? ImbueSound => SoundID.Item99;
 
 		public override float AOImbueDamage => 1.125f;
 		public override float AOImbueSpeed => 0.75f;
@@ -27,7 +27,7 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 		public override float AOScrollSize => 1.1f;
 		public override float AOScrollSpeed => 0.75f;
 
-		public override AODebuffRequirement[] ImbueDebuffs => [new(ModContent.BuffType<AOBleed>(), 60*10)];
+		public override AODebuffRequirement[] ImbueDebuffs => [new(ModContent.BuffType<AOBleed>(), 60 * 10)];
 		public override SynergyEffects Effects => new(
 			[
 				ModContent.BuffType<FreezingEffect>()
@@ -40,5 +40,34 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 				new MagicBuffMultiplier(BuffID.Venom,1.1f)
 			]
 		);
+		public override void SpawningEffects(Entity projectile)
+		{ 
+			for(int n = 0;n<10;n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X+(projectile.width*Main.rand.NextFloat()),projectile.position.Y+(projectile.height*Main.rand.NextFloat())),0,0,DustID.Mercury,(projectile.velocity.X*0.4f),(projectile.velocity.Y*0.4f),0,default,1f)];
+			}
+		}
+
+		public override void LingeringEffects(Entity projectile)
+		{
+			Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * Main.rand.NextFloat()), projectile.position.Y + (projectile.height * Main.rand.NextFloat())), 1, 1, DustID.SilverFlame, 0f, 0f, 0, default, 1f)];
+			spawnedDust.noGravity = true;
+			spawnedDust.noLight = true;
+		}
+		public override void ExplosionEffects(Entity projectile)
+		{
+			for (int n = 0; n < 3; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width / 2f), projectile.position.Y + (projectile.height / 2f)), 1, 1, DustID.Mercury, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 2f)];
+			}
+		}
+		public override void KillEffects(Entity projectile)
+		{
+			for (int n = 0; n < 30; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * Main.rand.NextFloat()), projectile.position.Y + (projectile.height * Main.rand.NextFloat())), 0, 0, DustID.Mercury, (2f * Main.rand.NextFloat() - 0.5f), (2f * Main.rand.NextFloat() - 0.5f), 0, default, 1f)];
+			}
+			SoundEngine.PlaySound(ImbueSound, projectile.position, null);
+		}
 	}
 }
