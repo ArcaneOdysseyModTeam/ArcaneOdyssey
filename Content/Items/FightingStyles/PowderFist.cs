173ade8a6ccb2fae5ed99ca0778f1ae719cd06e2
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-08 11:17:37 -0500

    Powder fist?

--- Content/Items/FightingStyles/PowderFist.cs
+++ Content/Items/FightingStyles/PowderFist.cs
@@ -12,6 +12,7 @@ using static ArcaneOdyssey.AOUtils;
 using ArcaneOdyssey.Content.Items.Materials;
 using ArcaneOdyssey.Content.Buffs.DOT;
 using Terraria.Audio;
+using ArcaneOdyssey.Content.Projectiles;
 
 namespace ArcaneOdyssey.Content.Items.FightingStyles
 {
@@ -77,6 +78,9 @@ namespace ArcaneOdyssey.Content.Items.FightingStyles
 				spawnedDust3.noGravity = true;
 				Dust spawnedDust2 = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + (projectile.width * Main.rand.NextFloat()), projectile.position.Y + (projectile.height * Main.rand.NextFloat())), 0, 0, DustID.Ash, (8f * Main.rand.NextFloat() - 0.5f), (8f * Main.rand.NextFloat() - 0.5f), 0, default, 4f)];
 				spawnedDust2.noGravity = true;
+			}
+			if (projectile is Projectile) {
+				Projectile proj = Projectile.NewProjectileDirect(Main.projectile[projectile.whoAmI].GetSource_FromThis(), projectile.Center, Vector2.Zero, ModContent.ProjectileType<PowderExplosion>(), 0, 3f, Main.projectile[projectile.whoAmI].owner, 0, Main.projectile[projectile.whoAmI].damage/2f);
 			}
 			SoundEngine.PlaySound(ImbueSound, projectile.position, null);
 		}
---
+++ Content/Projectiles/PowderExplosion.cs
@@ -0,0 +1,68 @@
+using ArcaneOdyssey.Content.Items.Base;
+using ArcaneOdyssey.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Terraria.ModLoader;
+using ArcaneOdyssey.Content.Projectiles.Base;
+using ArcaneOdyssey.Content.Items.Magic;
+using static ArcaneOdyssey.AOUtils;
+
+namespace ArcaneOdyssey.Content.Projectiles
+{
+    public class PowderExplosion : ModProjectile
+    {
+        public bool hasExploded = false;
+        public override void SetDefaults()
+        {
+            Projectile.ai[0] = 0;
+            Projectile.damage = 0;
+            Projectile.height = Projectile.width = 100;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+            hasExploded = false;
+        }
+        public override void AI()
+        {
+            Projectile.ai[0] += 1;
+            if (Projectile.ai[0] >= 60)
+            {
+                if (!hasExploded)
+                {
+                    float AOScrollSize = 1f;
+                    Projectile.damage = (int)Projectile.ai[1];
+                    for (int n = 0; n < 3; n++)
+                    {
+                        Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(Projectile.position.X + (Projectile.width / 2f), Projectile.position.Y + (Projectile.height / 2f)), 1, 1, DustID.Pixie, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 3f)];
+                        spawnedDust.noGravity = true;
+                        Dust spawnedDust2 = Main.dust[Dust.NewDust(new Vector2(Projectile.position.X + (Projectile.width / 2f), Projectile.position.Y + (Projectile.height / 2f)), 1, 1, DustID.Pixie, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 3f)];
+                        spawnedDust2.noGravity = true;
+                        Dust spawnedDust3 = Main.dust[Dust.NewDust(new Vector2(Projectile.position.X + (Projectile.width / 2f), Projectile.position.Y + (Projectile.height / 2f)), 1, 1, DustID.Ash, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 4f)];
+                        spawnedDust3.noGravity = true;
+                    }
+                }
+                hasExploded = true;
+                if (Projectile.ai[0] >= 120)
+                {
+                    Projectile.Kill();
+                }
+            }
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return false;
+        }
+	}
+}
