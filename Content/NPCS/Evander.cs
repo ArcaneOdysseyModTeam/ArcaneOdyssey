95223f6407a6b5956381ccb7c0b8c01381eb7f04
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-13 16:08:47 -0500

    Finally starting with evander

--- Content/NPCS/Evander.cs
+++ Content/NPCS/Evander.cs
@@ -25,8 +25,8 @@ namespace ArcaneOdyssey.Content.NPCS
 			NPC.damage = 100;
 			NPC.knockBackResist = 0f;
 			NPC.defense = 20;
-			NPC.height = 48;
-			NPC.width = 24;
+			NPC.height = 96;
+			NPC.width = 76;
 			NPC.HitSound = SoundID.NPCHit1;
 			NPC.DeathSound = SoundID.NPCDeath1;
 			NPC.friendly = false;
@@ -34,6 +34,7 @@ namespace ArcaneOdyssey.Content.NPCS
 			NPC.lavaImmune = false;
 			NPC.aiStyle = 0;
 			NPC.ai[0] = 0;
+			NPC.frame = 0;
 		}
 		private bool canJump = false;
 		public override void AI()
