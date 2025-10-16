ba553ef731e6881a87d48cf27ce016d50ec36b3a
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-16 17:32:37 -0500

    Added the headless head

---
+++ Content/Items/Vanity/HeadlessHead.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArcaneOdyssey.Content.Items.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class HeadlessHead : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            int equipSlotHead = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
+            ArmorIDs.Head.Sets.DrawHead[equipSlotHead] = false;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 50;
+            Item.accessory = true;
+            Item.value = 0;
+            Item.rare = ItemRarityID.Red;
+            Item.vanity = true;
+        }
+
+
+    }
+}
---
+++ Content/Items/Vanity/HeadlessHeadWithWig.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArcaneOdyssey.Content.Items.Vanity
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class HeadlessHeadWithWig : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            int equipSlotHead = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
+            ArmorIDs.Head.Sets.DrawHead[equipSlotHead] = false;
+            ArmorIDs.Head.Sets.DrawFullHair[equipSlotHead] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 50;
+            Item.accessory = true;
+            Item.value = 0;
+            Item.rare = ItemRarityID.Red;
+            Item.vanity = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().AddIngredient<HeadlessHead>().AddIngredient(ItemID.FamiliarWig).Register();
+        }
+    }
+}
