30bc466a282394cefdd901c8f9c045548fe82355
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-15 19:50:31 -0500

    Update BlindedEffect.cs

--- Content/Buffs/MagicMarks/BlindedEffect.cs
+++ Content/Buffs/MagicMarks/BlindedEffect.cs
@@ -16,6 +16,12 @@ namespace ArcaneOdyssey.Content.Buffs.MagicMarks
         private int stack = 1;
         public override void Update(NPC npc, ref int buffIndex)
         {
+            if (!Main.dedServ)
+            {
+                var dust = Dust.NewDustDirect(npc.position, npc.Hitbox.Width, npc.Hitbox.Height, DustID.YellowStarDust, 0f, 0f, 1, default, 1f);
+                dust.velocity *= 0.1f;
+                dust.noGravity = true;
+            }
             if (npc.HasBuff(Type))
             {
                 stack = GetAOBuffStack(npc, buffIndex); // stacks disappear over time
