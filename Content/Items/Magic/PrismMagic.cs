e1ea7cecb09fef24e913cafd02f1e03b903ea290
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-17 18:37:43 -0500

    Update PrismMagic.cs

--- Content/Items/Magic/PrismMagic.cs
+++ Content/Items/Magic/PrismMagic.cs
@@ -9,20 +9,32 @@ using ArcaneOdyssey.Content.Items.Materials;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using ArcaneOdyssey.Content.Projectiles.Magic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using ArcaneOdyssey.Content.Buffs.MagicMarks;
+using ArcaneOdyssey.Content.Buffs.DOT;
+using Terraria.Audio;
 using static ArcaneOdyssey.AOUtils;
 
 namespace ArcaneOdyssey.Content.Items.Magic
 {
 	public class PrismMagic : AOMagic
 	{
+		public static Color[] rainbowColors = [Color.Red, Color.Orange, Color.Green, Color.Blue, Color.Indigo, Color.Violet];
+		public override SoundStyle? ImbueSound => SoundID.Shatter;
+        public override Color ImbueColour => new(255,255,255);
         public override float AOImbueSpeed => 1f;
         public override float AOImbueDamage => 1f;
-        public override float AOImbueSize => 1f;
+		public override float AOImbueSize => 1f;
+		public override float AOScrollSpeed => 1f;
+        public override float AOScrollDamage => 1f;
+        public override float AOScrollSize => 1f;
 
-        public override AOImbuableTier ImbuableTier => AOImbuableTier.Lost;
+		public override AOImbuableTier ImbuableTier => AOImbuableTier.Lost;
+		public override AODebuffRequirement[] ImbueDebuffs => [new(ModContent.BuffType<BlindedEffect>(), 60*5),new(ModContent.BuffType<AOBleed>(), 60*10)];
 		
 		public override SynergyEffects Effects => new(
 			[ // these are debuffs cleared on hit
@@ -32,7 +44,60 @@ namespace ArcaneOdyssey.Content.Items.Magic
 				
 			]
 			);
-		
+		public override void SpawningEffects(Entity projectile)
+		{
+			int rainbowStep = (int)Main.GameUpdateCount;
+			for (int n = 0; n < 3; n++)
+            {
+                Dust dust = Dust.NewDustDirect(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[rainbowStep % 6], 1f);
+				dust.noGravity = true;
+				rainbowStep++;
+				Dust.NewDust(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.Glass, 0f, 0f, 0,default, 1f);
+            }
+		}
+		public override void LingeringEffects(Entity projectile)
+		{
+			Dust.NewDust(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.Glass, 0f, 0f, 0,default, 0.5f);
+			if (projectile is Projectile)
+            {
+				if (((Projectile)projectile).type == ModContent.ProjectileType<BeamSpell>())
+                {
+					Dust dust = Dust.NewDustDirect(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[((Projectile)projectile).numUpdates % 6], 1.4f);
+					dust.noGravity = true;
+                } else
+                {
+					Dust dust = Dust.NewDustDirect(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[Main.GameUpdateCount % 6], 1.4f);
+					dust.noGravity = true;
+                }
+            } else
+            {
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[Main.GameUpdateCount % 6], 1.4f);
+				dust.noGravity = true;
+            }
+		}
+		public override void KillEffects(Entity projectile)
+		{
+			int rainbowStep = (int)Main.GameUpdateCount;
+			for (int n = 0; n < 10; n++)
+			{
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[rainbowStep % 6], 2f);
+				dust.noGravity = true;
+				rainbowStep++;
+				Dust.NewDust(projectile.position, projectile.Hitbox.Width, projectile.Hitbox.Height, DustID.Glass, 0f, 0f, 0,default, 1.2f);
+			}
+			SoundEngine.PlaySound(ImbueSound, projectile.position, null);
+		}
+        public override void ExplosionEffects(Entity projectile)
+        {
+			int rainbowStep = (int)Main.GameUpdateCount;
+			Dust.NewDust(new Vector2(projectile.position.X + (projectile.width / 2f), projectile.position.Y + (projectile.height / 2f)), 1, 1, DustID.Glass, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 0.9f);
+			for (int n = 0; n < 10; n++)
+            {
+				Dust dust = Dust.NewDustDirect(new Vector2(projectile.position.X + (projectile.width / 2f), projectile.position.Y + (projectile.height / 2f)), 1, 1, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, rainbowColors[rainbowStep % 6], 1.3f);
+				dust.noGravity = true;
+				rainbowStep++;
+            }
+        }
         public override Dictionary<Type, int> Skills => new([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<PrismBlast>()), KeyValuePair.Create(typeof(PulsarSpell), ModContent.ProjectileType<PrismPulsar>()), KeyValuePair.Create(typeof(CannonSpell), ModContent.ProjectileType<PrismCannon>())]);
 		
 		public override void AddRecipes()
