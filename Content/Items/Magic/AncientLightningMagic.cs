e0625bd5f5eba2d2099ad47bb609188d9b3404d1
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-17 13:03:15 -0500

    Update AncientLightningMagic.cs

--- Content/Items/Magic/AncientLightningMagic.cs
+++ Content/Items/Magic/AncientLightningMagic.cs
@@ -93,7 +93,8 @@ namespace ArcaneOdyssey.Content.Items.Magic
 		{
 			for (int n = 0; n < 3; n++)
 			{
-				Dust.NewDust(new Vector2(projectile.position.X + (projectile.width / 2f), projectile.position.Y + (projectile.height / 2f)), 1, 1, DustID.CrimsonTorch, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 2.3f);
+				Dust dust = Dust.NewDustDirect(new Vector2(projectile.position.X + (projectile.width / 2f), projectile.position.Y + (projectile.height / 2f)), 1, 1, DustID.GemRuby, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 2.3f);
+				dust.noGravity = true;
 			}
 		}
 		public override void KillEffects(Entity projectile)
