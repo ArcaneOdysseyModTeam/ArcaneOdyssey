82ac49a154bb8db52cc1cbcb23e3291326b0a4d4
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-17 12:58:10 -0500

    Update AncientLightningMagic.cs

--- Content/Items/Magic/AncientLightningMagic.cs
+++ Content/Items/Magic/AncientLightningMagic.cs
@@ -105,6 +105,6 @@ namespace ArcaneOdyssey.Content.Items.Magic
 			SoundEngine.PlaySound(ImbueSound, projectile.position, null);
 		}
 		
-		public override Dictionary<Type, int> Skills => new([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<AncientLightningBlast>()), KeyValuePair.Create(typeof(CannonSpell), ModContent.ProjectileType<AncientLightningCannon>())]);
+		public override Dictionary<Type, int> Skills => new([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<AncientLightningBlast>()), KeyValuePair.Create(typeof(PulsarSpell), ModContent.ProjectileType<AncientLightningPulsar>()), KeyValuePair.Create(typeof(CannonSpell), ModContent.ProjectileType<AncientLightningCannon>())]);
 	}
 }
