46a679c7ea42de9a3f4ece1a6f0cead5c63b01a8
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-10-17 20:42:43 -0500

    Create GravityDust.cs

---
+++ VFX/Dusts/GravityDust.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArcaneOdyssey.VFX.Dusts
+{
+    public class GravityDust : ModDust
+    {
+        
+    }
+}
