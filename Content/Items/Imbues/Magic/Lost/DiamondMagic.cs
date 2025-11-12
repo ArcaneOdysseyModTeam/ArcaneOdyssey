026660d258c09363e71ee1422f058c28a18c93a7
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-11-12 15:45:53 -0600

    Diamond is done I guess

--- Content/Items/Imbues/Magic/Lost/DiamondMagic.cs
+++ Content/Items/Imbues/Magic/Lost/DiamondMagic.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using Terraria.ModLoader;
 using ArcaneOdyssey.Content.Items.Imbues.Magic.Normal;
 using Terraria.ID;
+using Terraria;
 using ArcaneOdyssey.Content.Buffs.DOT;
 using ArcaneOdyssey.Content.Buffs.MagicMarks;
 using Microsoft.Xna.Framework;
@@ -41,10 +42,40 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Lost
 				new MagicBuffMultiplier(ModContent.BuffType<Crystallized>(),1.125f)
 			]
 			);
+		public override void SpawningEffects(Entity projectile)
+		{
+            for (int n = 0; n < 10; n++)
+			{
+				Dust.NewDust(new Vector2(projectile.position.X+projectile.width*Main.rand.NextFloat(),projectile.position.Y+projectile.height*Main.rand.NextFloat()),0,0,DustID.GemSapphire,projectile.velocity.X*0.4f,projectile.velocity.Y*0.4f,0,default,1f);
+			}
+		}
+
+		public override void LingeringEffects(Entity projectile)
+		{
+			Dust spawnedDust = Main.dust[Dust.NewDust(new Vector2(projectile.position.X + projectile.width * Main.rand.NextFloat(), projectile.position.Y + projectile.height * Main.rand.NextFloat()), 1, 1, DustID.SilverFlame, 0f, 0f, 0, default, 1f)];
+			spawnedDust.noGravity = true;
+			spawnedDust.noLight = true;
+		}
+		public override void ExplosionEffects(Entity projectile)
+		{
+			for (int n = 0; n < 3; n++)
+			{
+				Dust.NewDust(projectile.Center, 1, 1, DustID.GemSapphire, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 3f);
+			}
+		}
+
+		public override void KillEffects(Entity projectile)
+		{
+			for (int n = 0; n < 30; n++)
+			{
+				Dust.NewDust(new Vector2(projectile.position.X + projectile.width * Main.rand.NextFloat(), projectile.position.Y + projectile.height * Main.rand.NextFloat()), 0, 0, DustID.GemSapphire, 2f * (Main.rand.NextFloat() - 0.5f), 2f * (Main.rand.NextFloat() - 0.5f), 0, default, 1f);
+			}
+			SoundEngine.PlaySound(ImbueSound, projectile.position, null);
+		}
 		public override Dictionary<Type, int> Skills => new([KeyValuePair.Create(typeof(BlastSpell), ModContent.ProjectileType<DiamondBlast>()), KeyValuePair.Create(typeof(PulsarSpell), ModContent.ProjectileType<DiamondPulsar>()), KeyValuePair.Create(typeof(CannonSpell), ModContent.ProjectileType<DiamondCannon>())]);
 		
 		public override void AddRecipes() {
-            CreateLostRecipe(typeof(CrystalMagic), typeof(EarthMagic));
+            CreateLostRecipe(typeof(CrystalMagic), typeof(EarthMagic),typeof(MetalMagic),typeof(SandMagic),typeof(GlassMagic),typeof(MagmaMagic),typeof(WoodMagic));
         }
 	}
 }
