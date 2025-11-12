9fe359cc5863658a9d1382551228f4eb4e61f8d0
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2025-11-12 15:49:26 -0600

    buffed diamond a bit

--- Content/Items/Imbues/Magic/Lost/DiamondMagic.cs
+++ Content/Items/Imbues/Magic/Lost/DiamondMagic.cs
@@ -19,12 +19,12 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Lost
 {
 	public class DiamondMagic : AOMagic
     {
-        public override float AOImbueSpeed => 1f;
+        public override float AOImbueSpeed => 1.13f;
 		public override float AOImbueSize => 1.11f;
-		public override float AOImbueDamage => 1.25f;
-		public override float AOScrollSpeed => 1f;
+		public override float AOImbueDamage => 1.3f;
+		public override float AOScrollSpeed => 1.13f;
 		public override float AOScrollSize => 1.15f;
-		public override float AOScrollDamage => 1.25f;
+		public override float AOScrollDamage => 1.3f;
 		public override Color ImbueColour => new(50,255,255);
         public override AOImbuableTier ImbuableTier => AOImbuableTier.Lost;
         public override SoundStyle? ImbueSound => SoundID.Shatter;
