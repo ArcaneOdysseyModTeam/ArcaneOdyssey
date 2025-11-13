12beadcea4b4e6ba638a8b8d4c262eeb2bfe8704
Guard <guardboi24@gmail.com>
2025-11-13 14:10:07 -0800

    unobtainable bow

---
+++ Content/Items/Weapons/StormCaller.cs
@@ -0,0 +1,39 @@
+﻿using ArcaneOdyssey.Content.Buffs.MagicMarks;
+using ArcaneOdyssey.Content.Items.Base;
+using ArcaneOdyssey.Content.Items.Weapons.Bronze;
+using ArcaneOdyssey.Content.Projectiles.Base;
+using ArcaneOdyssey.Content.Projectiles.Weapons;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static ArcaneOdyssey.AOUtils;
+
+namespace ArcaneOdyssey.Content.Items.Weapons
+{
+	public class StormCaller : AORangedOrMeleeWeapon
+	{
+		public override float AODamage => 0.9f;
+		public override float AOSize => 1.1f;
+		public override float AOSpeed => 1.15f;
+		public override int AOValue => 120;
+		public override AOItemTiers AOWeaponTier => AOItemTiers.Average;
+		public override AORarities AORarity => AORarities.Rare;
+		public override SoundStyle UseSound => SoundID.Item5;
+
+		public override void SetDefaults()
+		{
+			base.SetDefaults();
+			Item.noMelee = true;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.DamageType = DamageClass.Ranged;
+			      Item.width = 18;
+            Item.height = 56;
+            Item.autoReuse = true;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.shootSpeed = 12f;
+            Item.useAmmo = AmmoID.Arrow;
+		}
+    }
+    }
