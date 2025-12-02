69d21a8c9cde1b98a3f076fa6b3ec33d063fc565
Guard Boi <105892653+guardboi@users.noreply.github.com>
2025-12-02 00:20:16 -0800

    storm magic skeleton

---
+++ Content/Items/Imbues/Magic/Lost/StormMagic.cs
@@ -0,0 +1,13 @@
+﻿using ArcaneOdyssey.Content.Items.Base;
+
+namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Lost
+{
+	public class StormMagic : AOMagic
+	{
+		public override AOImbuableTier ImbuableTier => AOImbuableTier.Lost;
+		public override float AOImbueDamage => 1f;
+		public override float AOImbueSpeed => 1f;
+		public override float AOImbueSize => 1f;
+		
+	}
+}
