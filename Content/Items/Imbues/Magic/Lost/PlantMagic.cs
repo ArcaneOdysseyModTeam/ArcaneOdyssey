67f51db40281837c21a0171de57aae7c273273cd
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2026-01-11 19:07:03 -0600

    Changed plant magic's dust, fixing the gore overload issue

--- Content/Items/Imbues/Magic/Lost/PlantMagic.cs
+++ Content/Items/Imbues/Magic/Lost/PlantMagic.cs
@@ -10,9 +10,39 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Lost
 		public override AOImbuableTier ImbuableTier => AOImbuableTier.Lost;
 		public override Color ImbueColour => Color.ForestGreen;
 
-		public override void LingeringEffects(Entity entity)
+		public override void SpawningEffects(Entity projectile)
 		{
-			Gore.NewGore(entity.GetSource_FromThis(), entity.Center, entity.velocity / 4f, GoreID.TreeLeaf_Normal);
+			for (int n = 0; n < 3; n++)
+			{
+				Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Pearlwood, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 0, default, 1.5f);
+			}
+		}
+		public override void LingeringEffects(Entity projectile)
+		{
+			Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Pearlwood, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 0, default, 1f);
+			Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.GrassBlades, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 0, default, 1.5f);
+		}
+		public override void ExplosionEffects(Entity projectile)
+		{
+			for (int n = 0; n < 3; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(projectile.Center, 0, 0, DustID.Pearlwood, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 2.5f)];
+				spawnedDust.noGravity = true;
+				Dust spawnedDust2 = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.GrassBlades, 8f * (Main.rand.NextFloat() - 0.5f), 8f * (Main.rand.NextFloat() - 0.5f), 0, default, 1.5f)];
+				spawnedDust2.noGravity = true;
+			}
+		}
+
+		public override void KillEffects(Entity projectile)
+		{
+			for (int n = 0; n < 10; n++)
+			{
+				Dust spawnedDust = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Pearlwood, 8f * (Main.rand.NextFloat() - 0.5f), 8f * (Main.rand.NextFloat() - 0.5f), 0, default, 2f)];
+				spawnedDust.noGravity = true;
+				Dust spawnedDust2 = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.GrassBlades, 8f * (Main.rand.NextFloat() - 0.5f), 8f * (Main.rand.NextFloat() - 0.5f), 0, default, 1.5f)];
+				spawnedDust2.noGravity = true;
+			}
+			SoundEngine.PlaySound(ImbueSound, projectile.Center, null);
 		}
 	}
 }
