34c12e6304bb143e38f0f1cddd9e42a4ef1d728f
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2026-01-11 19:08:21 -0600

    Altered wood magic's vfx

--- Content/Items/Imbues/Magic/Normal/WoodMagic.cs
+++ Content/Items/Imbues/Magic/Normal/WoodMagic.cs
@@ -46,7 +46,6 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Normal
 		public override void LingeringEffects(Entity projectile)
 		{
 			Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Pearlwood, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 0, default, 1f);
-			Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.GrassBlades, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 0, default, 1.5f);
 		}
 		public override void ExplosionEffects(Entity projectile)
 		{
@@ -54,8 +53,6 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Normal
 			{
 				Dust spawnedDust = Main.dust[Dust.NewDust(projectile.Center, 0, 0, DustID.Pearlwood, (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), (Main.rand.NextFloat() - 0.5f) * (15f * AOScrollSize), 0, default, 2.5f)];
 				spawnedDust.noGravity = true;
-				Dust spawnedDust2 = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.GrassBlades, 8f * (Main.rand.NextFloat() - 0.5f), 8f * (Main.rand.NextFloat() - 0.5f), 0, default, 1.5f)];
-				spawnedDust2.noGravity = true;
 			}
 		}
 
@@ -65,8 +62,6 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Normal
 			{
 				Dust spawnedDust = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Pearlwood, 8f * (Main.rand.NextFloat() - 0.5f), 8f * (Main.rand.NextFloat() - 0.5f), 0, default, 2f)];
 				spawnedDust.noGravity = true;
-				Dust spawnedDust2 = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.GrassBlades, 8f * (Main.rand.NextFloat() - 0.5f), 8f * (Main.rand.NextFloat() - 0.5f), 0, default, 1.5f)];
-				spawnedDust2.noGravity = true;
 			}
 			SoundEngine.PlaySound(ImbueSound, projectile.Center, null);
 		}
