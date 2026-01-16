1ae8ed21375a57b07ebac32c5dc3832420eeac4b
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2026-01-15 20:14:54 -0600

    WOOOO MORE AETHER CHANGES

--- Content/Projectiles/Magic/Effects/AetherExplosion.cs
+++ Content/Projectiles/Magic/Effects/AetherExplosion.cs
@@ -30,7 +30,7 @@ namespace ArcaneOdyssey.Content.Projectiles.Magic.Effects
 			{
 				Count++;
 				//if (ArcaneOdysseyConfig.Instance.ProjectileSizes)
-				BaseScale = MathHelper.Clamp((projectile.width + projectile.height) * projectile.scale / 2f / Projectile.width, .5f, 3f);
+				BaseScale = MathHelper.Clamp((projectile.width + projectile.height) * projectile.scale / 2f / Projectile.width, .37f, 1.3f);
 			}
 			else
 			{
