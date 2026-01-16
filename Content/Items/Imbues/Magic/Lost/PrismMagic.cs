be9b1f769700e608662a09c177da42bdab66e311
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2026-01-15 22:19:16 -0600

    Fixed Prism magic

--- Content/Items/Imbues/Magic/Lost/PrismMagic.cs
+++ Content/Items/Imbues/Magic/Lost/PrismMagic.cs
@@ -1,3 +1,4 @@
+using System;
 using ArcaneOdyssey.Content.Buffs.DOT;
 using ArcaneOdyssey.Content.Buffs.MagicMarks;
 using ArcaneOdyssey.Content.Items.Base;
@@ -67,7 +68,7 @@ namespace ArcaneOdyssey.Content.Items.Imbues.Magic.Lost
 			{
 				if (projectile.extraUpdates > 0)
 				{
-					Dust dust = Dust.NewDustDirect(entity.position, entity.Hitbox.Width, entity.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[projectile.numUpdates % 3], 1.4f);
+					Dust dust = Dust.NewDustDirect(entity.position, entity.Hitbox.Width, entity.Hitbox.Height, DustID.AncientLight, (Main.rand.NextFloat() - 0.5f) * 3f, (Main.rand.NextFloat() - 0.5f) * 3f, 0, rainbowColors[Math.Abs(projectile.numUpdates) % 3], 1.4f);
 					dust.noGravity = true;
 				}
 				else
