2e14583009c1e7b24221861c4d6bab68bd6ef1ee
Guard <guardboi24@gmail.com>
2026-01-25 16:53:39 -0800

    what am i even doing

--- Content/Items/Weapons/ScimitarofStorm.cs
+++ Content/Items/Weapons/ScimitarofStorm.cs
@@ -1,8 +1,8 @@
 ﻿using ArcaneOdyssey.Content.Items.Base;
-using ArcaneOdyssey.Content.Projectiles.Weapons.Abilities;
+using ArcaneOdyssey.Content.Projectiles.Weapons;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -26,30 +26,22 @@ namespace ArcaneOdyssey.Content.Items.Weapons
 			base.SetDefaults();
 			Item.useTime = Item.useAnimation / 2;
 			Item.useStyle = ItemUseStyleID.Swing;
-			Item.shoot = ModContent.ProjectileType<TwinCrescent>();
-			Item.shootSpeed = 8f * AOSpeed;
+			Item.noUseGraphic = true;
+			Item.DamageType = DamageClass.MeleeNoSpeed;
+			Item.shoot = ModContent.ProjectileType<ScimitarofStormProjectile>();
 		}
 
 		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
 		{
-			damage /= 3;
-			knockback /= 3;
+			damage /= 2;
+			knockback /= 2;
 		}
 
 		public override WeaponAbility? Ability => new(Mod, "Twin Crescents", "Slash both blades one after the other, sending two flying slashes towards the target", Color.Gold);
 
-		public Texture2D Sprite => ModContent.Request<Texture2D>(Texture).Value;
-		public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 		{
-			spriteBatch.Draw(Sprite, position, frame, drawColor, 0, origin, scale, SpriteEffects.FlipHorizontally, 0f);
-		}
-
-		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
-		{
-			Main.GetItemDrawFrame(Type, out var itemTexture, out var itemFrame);
-			Vector2 drawOrigin = itemFrame.Size() / 2f;
-			Vector2 drawPosition = Item.Bottom - Main.screenPosition - new Vector2(0, drawOrigin.Y);
-			spriteBatch.Draw(itemTexture, drawPosition, itemFrame, lightColor, rotation, drawOrigin, scale, SpriteEffects.FlipHorizontally, 0f);
+			return base.Shoot(player, source, position, velocity, type, damage, knockback);
 		}
 	}
 }
