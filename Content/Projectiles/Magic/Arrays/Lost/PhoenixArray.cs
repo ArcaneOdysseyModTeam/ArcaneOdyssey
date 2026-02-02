30f0e095853fe5dce065e2e279b2a164015ad2fe
Guard <guardboi24@gmail.com>
2026-02-02 13:18:50 -0800

    fixed phoenix being upside down

--- Content/Projectiles/Magic/Arrays/Lost/PhoenixArray.cs
+++ Content/Projectiles/Magic/Arrays/Lost/PhoenixArray.cs
@@ -1,4 +1,5 @@
 using ArcaneOdyssey.Content.Projectiles.Base;
+using System;
 using Terraria;
 
 namespace ArcaneOdyssey.Content.Projectiles.Magic.Arrays.Lost
@@ -8,6 +9,12 @@ namespace ArcaneOdyssey.Content.Projectiles.Magic.Arrays.Lost
 		public override void SetStaticDefaults()
 		{
 			Main.projFrames[Type] = 4;
+		}
+
+		public override void Rotate()
+		{
+			Projectile.rotation = Projectile.velocity.ToRotation();
+			Projectile.spriteDirection = Math.Sign(Projectile.velocity.X);
 		}
 	}
 }
