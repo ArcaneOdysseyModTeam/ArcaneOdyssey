1409049730124d77a80ce40f40d8a0c1221b66c2
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2026-07-29 14:26:17 -0500

    Prep for storm of arrows

---
+++ Projectiles/Enemies/EliusArrowStorm.cs
@@ -0,0 +1,28 @@
+﻿using ArcaneOdyssey.Imbues.Base;
+using ArcaneOdyssey.Imbues.Relics;
+using ArcaneOdyssey.Projectiles.Base;
+using ArcaneOdyssey.Projectiles.Relics;
+using Terraria.Audio;
+
+namespace ArcaneOdyssey.Projectiles.Enemies
+{
+	public class EliusArrowStorm : BaseProjectile
+	{
+		public override void SetDefaults()
+		{
+			base.SetDefaults();
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.timeLeft = 500;
+			Projectile.hostile = true;
+			Projectile.friendly = false;
+			Projectile.penetrate = -1;
+			Projectile.height = Projectile.width = 70;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+		}
+		public override void AI()
+		{
+			Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
+		}
+	}
+}
