c4c97a90ba02a13cdb757e108d4b6be42c89accb
Redbirb <152514217+RedBirb1508@users.noreply.github.com>
2026-07-30 20:09:50 -0500

    Update EliusPlacedExplosion.cs

--- Projectiles/Enemies/EliusPlacedExplosion.cs
+++ Projectiles/Enemies/EliusPlacedExplosion.cs
@@ -28,7 +28,7 @@ namespace ArcaneOdyssey.Projectiles.Enemies
 		public override void OnKill(int timeLeft)
 		{
 			Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.Center+new Vector2(0,-1000),Vector2.Zero,ModContent.ProjectileType<EliusTrail>(),0,0f,-1,Projectile.Center.X,Projectile.Center.Y);
-			Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.Center,Vector2.Zero,ModContent.ProjectileType<EliusExplosion>(),50,1f,-1);
+			Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.Center,Vector2.Zero,ModContent.ProjectileType<EliusExplosion>(),50,8f,-1);
 			SoundEngine.PlaySound(SoundID.Thunder,Projectile.Center);
 			base.OnKill(timeLeft);
 		}
